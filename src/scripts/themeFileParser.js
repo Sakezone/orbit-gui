ba0e0d4ced8443769b4605ddf8bf6111d35f7906
Jarod Castillo <Jarodc2017@gmail.com>
2020-09-12 03:53:23 -0700

    version matching implemented

--- src/scripts/themeFileParser.js
+++ src/scripts/themeFileParser.js
@@ -1,20 +1,65 @@
+const https = require('https');
 const fs = require('fs');
+const path = require('path');
 
-
-// TESTING REASONS
 // Find version number.
+// TODO: We don't want this to be spammable, set a gate or something.
 function checkOrbitFileVersion() {
-    var pathName = 'F:/orbit.theme.css';
+    /* Write Start */
+    // Saving the orbit file from github into the cache folder
+    let file = fs.createWriteStream(path.join(process.env.APPDATA, "/orbit/cache/orbit.theme.css"), 'utf8');
+    https.get('https://raw.githubusercontent.com/Sakezone/orbit/master/theme%20file/orbit.theme.css', response => {
+        response.pipe(file);
 
-    fs.readFile(pathName, 'utf8', function (err,data) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(data);
-    });
+        file.on('finish', () => {
+            file.close();
+            console.log("temporary file written to cache folder.");
+
+            /* Read Start */
+            fs.readFile(path.join(process.env.APPDATA, "/orbit/cache/orbit.theme.css"),
+                'utf8', function (err, githubData) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    let arrayOfGithubData = githubData.split(/("([^"]|"")*")/g);
+                    // Hard coded.
+                    console.log("Recent " + arrayOfGithubData[22]);
+
+                    /* Start */
+                    fs.readFile(path.join(process.env.APPDATA, "/BetterDiscord/themes/orbit.theme.css"),
+                        'utf8', function (err, data) {
+                            if (err) {
+                                // TODO: Refer to install orbit theme function
+                                return console.log(err);
+                            }
+                            let arrayOfData = data.split(/("([^"]|"")*")/g);
+                            // Hard coded.
+                            console.log("Local " + arrayOfData[22]);
+
+                            /* COMPARING VERSION NUMBERS */
+                            if (arrayOfGithubData[22] + arrayOfData[22]) {
+                                console.log("Versions match :^) ");
+                                // TODO: Throw a boolean back to let front end know we are good to go :^)
+                            } else {
+                                console.log("Versions don't match :^( ");
+                                // TODO: Refer to install orbit theme function
+                            }
+                        }
+                    )
+                    /* End */
+                }
+            )
+            /* End of Read */
+
+        })
+    }).on("error", err => {
+        console.log(err.message);
+    })
+    /* Write End */
+
+
+
+
 }
 
 exports.checkOrbitFileVersion = checkOrbitFileVersion;
-
-
-// Find locations for all variables.
