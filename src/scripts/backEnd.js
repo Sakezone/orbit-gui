91ffe789448fc2c0b65805923a9f79792a4ffa20
Jarod Castillo <Jarodc2017@gmail.com>
2020-09-11 19:13:45 -0700

    rebasing test 2

--- src/scripts/backEnd.js
+++ src/scripts/backEnd.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 function InstallTheme() {
+<<<<<<< HEAD
    // let file = fs.createWriteStream("C:/Users/axemi/AppData/Roaming/BetterDiscord/themes/orbit.theme.css");
    let file = fs.createWriteStream(path.join(process.env.APPDATA, "/BetterDiscord/themes/orbit.theme.css"));
    https.get('https://raw.githubusercontent.com/Sakezone/orbit/master/theme%20file/orbit.theme.css', response => {
@@ -16,6 +17,9 @@ function InstallTheme() {
     }).on("error", err => {
         console.log(err.message);
     })
+=======
+    console.log("testing rebase 2!");
+>>>>>>> f0c8ff7... rebasing test 2
 }
 
 exports.InstallTheme = InstallTheme;
